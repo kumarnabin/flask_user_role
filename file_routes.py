e5fd15c56974e577e948a799df131e6b557eb2e3
kumarnabin <touchnabin@gmail.com>
2023-08-28 22:10:02 +0545

    file displayed

--- file_routes.py
+++ file_routes.py
@@ -1,13 +1,19 @@
 import json
 import os
 
-from flask import Blueprint, render_template, request, redirect, flash
+from flask import Blueprint, render_template, request, redirect, flash, send_file
 from werkzeug.utils import secure_filename
 
 from config import app, csrf
 from forms import FileForm, FileApiForm
 
 file_routes = Blueprint('file_routes', __name__)
+
+
+@app.route('/images/<filename>')
+def serve_image(filename):
+    image_path = app.config['UPLOAD_FOLDER'] + '/' + filename
+    return send_file(image_path, mimetype='image/jpeg')  # Adjust mimetype based on image type
 
 
 @file_routes.route('/file/upload', methods=['GET', 'POST'])
@@ -18,6 +24,7 @@ def upload_file():
             f = request.files['file']
             f.save(os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(f.filename)))
             flash('File uploaded successfully!', 'success')
+            flash(f.filename, 'file_name')
             return redirect("/file/upload")
         for field, errors in form.errors.items():
             for error in errors:
